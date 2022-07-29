a5d873d0070032066f3ddc029af5261b4e7bb229
irvan taufik <irvantaufik28@gmail.com>
2022-07-29 20:46:25 +0700

    Validation Kategori & Produk

--- routes/kategori.js
+++ routes/kategori.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const router =express.Router()
 const kategori = require('../controllers/kategori.controller')
-
+const Validation = require('../validation/kategori/kategori.validation')
 
 
 
 router.get('/', kategori.findAll)
 router.get('/:id', kategori.findOne)
-router.post('/', kategori.create)
-router.put('/:id', kategori.update)
+router.post('/',Validation.createKategori, kategori.create)
+router.put('/:id',Validation.createKategori, kategori.update)
 router.delete('/:id', kategori.delete)
 
 module.exports = router
